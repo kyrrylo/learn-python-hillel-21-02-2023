18066f69d9494a9363480ed2796764966fed42c1
kyrrylo <kyrrylo@gmail.com>
2023-02-21 20:18:52 +0100

    first lecture

--- my_first_script.py
+++ my_first_script.py
@@ -1,3 +1,15 @@
 """
 This is python file
 """
+# print - команда для вывода информации в консоль
+print(5 + 5)
+print(5 / 2)
+print('Hello World!')
+print("Hello World!")
+print("Hello World!")
+
+# input - команда для получения ввода пользователя из консоли
+name = input("WHat is your name? ")
+
+# f - format, форматируемая строка, позволяет включать в себя значения переменных
+print(f'Hello {name}!')
