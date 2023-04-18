ed90dcc46d5c33ece07879c1f8811f7318bdf5b2
kyrrylo <kyrrylo@gmail.com>
2023-04-18 20:32:43 +0200

    декораторы, работа с диаграммами классов

--- lesson_16/car.py
+++ lesson_16/car.py
@@ -1,12 +1,6 @@
-from typing import List, Dict
+from car_technician import CarTechnician
 
-# Наследование
-# Полиморфизм (Взаимозаменяемость)
-# Инкапсуляция
 
-# Агрегация или Композиция - один класс использует объект другого класса
-
-# SOLID - принципы проектирования (для ООП и не только)
 class Car:
     def __init__(self, model: str, total_km: int, fuel_tank_volume: int, fuel_consumption: float,
                  fuel: float, fuel_type: str):
@@ -16,6 +10,11 @@ class Car:
         self._fuel_tank_volume = fuel_tank_volume
         self.__fuel = fuel
         self._fuel_type = fuel_type
+
+        self.car_technicians = list()
+
+    def add_technician(self, car_technician: CarTechnician):
+        self.car_technicians.append(car_technician)
 
     @property
     def fuel_volume(self):
@@ -53,41 +52,3 @@ class Car:
             print(self)
         else:
             print(f'Необходимо {fuel_needed_for_journey}л топлива для поездки, в машине только {self.__fuel}л')
-
-
-class GasStation:
-    def __init__(self, fuel_prices: Dict[str, float]):
-        # GasStation агрегирует в себя тип данных dict
-        self.fuel_prices = fuel_prices
-
-    # метод класса GasStation принимает объект другого класса (Car)
-    # может быть объект своего класса
-
-    # сигнатура метода/функции (даже если не в классе):
-    # имя функции/метода; перечень входных параметров (и их типов); тип возвращаемого значения
-    def fuel_to_full(self, car: Car):
-        """
-        Заправляем до тех пор, пока в топливном баке автомобиля есть место
-        :param car:
-        :return:
-        """
-        fuel_type = car._fuel_type.lower()
-        if fuel_type in self.fuel_prices:
-            # узнаём сколько быо заправлено
-            fuel_volume = car.fuel()
-            price = self.fuel_prices[fuel_type] * fuel_volume
-            print(f'Заправлено {fuel_type}: {fuel_volume}л, цена: {price:.2f} UAH')
-            print(car)
-
-
-if __name__ == '__main__':
-    c = Car('Lanos', total_km=150000, fuel_tank_volume=50, fuel_consumption=7, fuel=30, fuel_type="Газ")
-    c.drive(100)
-    c.drive(300)
-    c.drive(50)
-
-    okko = GasStation({'газ': 22, 'бензин 95': 49, 'дизель': 52})
-    wog = GasStation({'газ': 21, 'бензин 95': 50, 'дизель': 52})
-
-    wog.fuel_to_full(c)
-    c.drive(50)
